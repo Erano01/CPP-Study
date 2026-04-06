2e74786dd89a51a4e00d9685abb0e5263a9cefc3
Erano01 <muhammedalisert01@gmail.com>
2026-04-06 21:30:21 +0300

    Feat: tour of c++ book examples and my comments

--- The-CPP-Language/part1-chapter2-basics/src/main.cpp
+++ The-CPP-Language/part1-chapter2-basics/src/main.cpp
@@ -1,6 +1,62 @@
 #include <iostream>
 
+
+// In C++ there are 2 different variable types: user-defined types and built-in types. 
+// Built-in types are defined by the compiler, such as int, double, char, etc. User-defined types are defined by the programmer, such as classes, structs, enums, etc. 
+
+// This is built-in type and object.
+// In C++, the definition of an object is: a memory region that holds the value of a specific type.
+// The "object = class instance" definition that you might remember from Java/C# is part of those languages' own conventions.
+int exampleOfObject = 5;
+
+// Each built-in type has a specific size in bytes, which can vary depending on the platform and compiler. However, there are some common sizes for built-in types:
+// bool,char -> 1 byte, 
+// short -> 2 bytes, 
+// int,float -> 4 bytes, 
+// long,long long,double -> 8 bytes. 
+
+// This custom Vector and std:Vector is user-defined type. Standard library types are also user-defined types.
+// Everything the user can do, the standard library should be able to do with the same tools. There shouldn't be any need for compiler magic.
+struct Vector {
+    int sz;
+    double* elem;
+};
+
+// These are C++'s immutable structures.
+const int MAX_SIZE = 1000; // equivalent to static final in Java/C#.
+constexpr double PI = 3.14159; // constexpr is a more powerful version of const. Evaluated at compile-time.
+
+// For a function to be `constexpr`, it needs to be quite simple: just a `return` statement that calculates a value.
+constexpr int square(int x) {
+    return x * x;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    
+    // << -> put to operator
+    std::cout << "Do you want to calculate the area of a circle? (y/n): ";
+
+    char answer;
+    // >> -> get from operator
+    std::cin >> answer; 
+
+    switch (answer)
+    {
+    case 'y':
+        std::cout << "Enter the radius of the circle: ";
+        double radius;
+        std::cin >> radius;
+        double area = PI * square(radius);
+        std::cout << "The area of the circle is: " << area << std::endl;
+        break;
+    case 'n':
+        std::cout << "Okay, maybe next time!" << std::endl;
+        break;
+    default:
+        std::cout << "Invalid input. Please enter 'y' or 'n'." << std::endl;
+        break;
+    }
+
+
     return 0;
 }
