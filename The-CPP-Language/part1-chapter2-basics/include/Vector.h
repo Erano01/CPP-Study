399588171fbd2298751aa78b260075a75fc3349e
Erano01 <muhammedalisert01@gmail.com>
2026-04-09 23:20:40 +0300

    Feat: exception handling

--- The-CPP-Language/part1-chapter2-basics/include/Vector.h
+++ The-CPP-Language/part1-chapter2-basics/include/Vector.h
@@ -1,15 +1,12 @@
 
-namespace erano_collections
+class Vector
 {
-    class Vector
-    {
-    public:
-        Vector(int s);
-        double &operator[](int i);
-        int size();
+public:
+    Vector(int s);
+    double &operator[](int i);
+    int size();
 
-    private:
-        int sz; // elem points to an array of sz doubles
-        double *elem;
-    };
-}
+private:
+    int sz; // elem points to an array of sz doubles
+    double *elem;
+};
--- The-CPP-Language/part1-chapter2-basics/src/Vector.cpp
+++ The-CPP-Language/part1-chapter2-basics/src/Vector.cpp
@@ -1,4 +1,7 @@
 #include "Vector.h"
+#include <stdexcept>
+
+using namespace std;
 
 Vector::Vector(int s) 
     :elem{new double[s]}, sz{s}
@@ -6,7 +9,10 @@ Vector::Vector(int s)
 }
 
 double& Vector::operator[](int i) {
+    
+    if (i < 0 || size() <= i) throw out_of_range{"Vector::operator[]"};
     return elem[i];
+
 }
 
 int Vector::size(){
